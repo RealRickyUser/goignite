35a032637c840500ff0a59bd8bda6b3ead2fb171
user <rickyuser@gmail.com>
2019-02-24 16:30:43 +0300

    Add unit-tests fot binary writer

--- ignite_test.go
+++ ignite_test.go
@@ -1,42 +1,34 @@
-package goignite_test
+package goignite
 
 import (
-	"fmt"
-	"github.com/RealRickyUser/goignite"
+	"bufio"
+	"bytes"
+	"testing"
 )
 
-func ExampleIgniteClient_GetOrCreateCache() {
-	client := goignite.NewClient("127.0.0.1:10800")
-	err := client.Connect()
-	if err != nil {
-		panic(err)
-	}
-	defer client.Close()
-	err = client.GetOrCreateCache("new_cache_name")
-	if err != nil {
-		fmt.Println(err)
-	}
+func TestWriteByte(t *testing.T) {
+	testWriteAny(t, byte(0x12), []byte{0x12})
 }
 
-func ExampleIgniteClient_Connect() {
-	client := goignite.NewClient("127.0.0.1:10800")
-	err := client.Connect()
-	if err != nil {
-		panic(err)
-	}
-	defer client.Close()
+func TestWriteShort(t *testing.T) {
+	testWriteAny(t, uint16(0x3412), []byte{0x12, 0x34})
 }
 
-func ExampleIgniteClient_GetCacheNames() {
-	client := goignite.NewClient("127.0.0.1:10800")
-	err := client.Connect()
-	if err != nil {
-		panic(err)
+func TestWriteInt(t *testing.T) {
+	testWriteAny(t, uint32(0x78563412), []byte{0x12, 0x34, 0x56, 0x78})
+}
+
+func TestWriteLong(t *testing.T) {
+	testWriteAny(t, uint64(0x6473829178563412), []byte{0x12, 0x34, 0x56, 0x78, 0x91, 0x82, 0x73, 0x64})
+}
+
+func testWriteAny(t *testing.T, data interface{}, expected []byte) {
+	buff := new(bytes.Buffer)
+	writer := bufio.NewWriter(buff)
+	write(writer, data)
+	writer.Flush()
+	received := buff.Bytes()
+	if !bytes.Equal(received, expected) {
+		t.Errorf("'write' makes incorrect long value, expected: %d, actual %d", expected, data)
 	}
-	defer client.Close()
-	caches, err := client.GetCacheNames()
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(caches)
 }
